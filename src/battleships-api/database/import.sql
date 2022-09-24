026e48f25df13556cce32d152f3d6d6b9ff705e9
sarunas <sarunas.palianskas@ktu.edu>
2022-09-24 18:49:07 +0300

    add database import script

---
+++ src/battleships-api/database/import.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 24, 2022 at 05:45 PM
+-- Server version: 10.9.3-MariaDB
+-- PHP Version: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `battleships`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matches`
+--
+
+CREATE TABLE `matches` (
+  `matchId` varchar(36) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `matches`
+--
+
+INSERT INTO `matches` (`matchId`, `name`) VALUES
+('0fd9bd9f-3c1f-11ed-b1d8-2cf05d21804e', 'test');
+
+--
+-- Triggers `matches`
+--
+DELIMITER $$
+CREATE TRIGGER `before_insert_matches` BEFORE INSERT ON `matches` FOR EACH ROW BEGIN
+  IF new.matchId IS NULL OR new.matchId = '' THEN
+    SET new.matchId = uuid();
+  END IF;
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `matches`
+--
+ALTER TABLE `matches`
+  ADD PRIMARY KEY (`matchId`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

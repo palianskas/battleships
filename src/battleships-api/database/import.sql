3e0005a5cca943d77f3d854f031e7d200a322305
sarunas <sarunas.palianskas@ktu.edu>
2022-09-25 12:51:37 +0300

    api: update database import script

--- src/battleships-api/database/import.sql
+++ src/battleships-api/database/import.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 24, 2022 at 05:45 PM
+-- Generation Time: Sep 25, 2022 at 11:50 AM
 -- Server version: 10.9.3-MariaDB
 -- PHP Version: 8.0.11
 
@@ -28,28 +28,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `matches` (
-  `matchId` varchar(36) NOT NULL,
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `matches`
---
-
-INSERT INTO `matches` (`matchId`, `name`) VALUES
-('0fd9bd9f-3c1f-11ed-b1d8-2cf05d21804e', 'test');
-
---
--- Triggers `matches`
---
-DELIMITER $$
-CREATE TRIGGER `before_insert_matches` BEFORE INSERT ON `matches` FOR EACH ROW BEGIN
-  IF new.matchId IS NULL OR new.matchId = '' THEN
-    SET new.matchId = uuid();
-  END IF;
-END
-$$
-DELIMITER ;
 
 --
 -- Indexes for dumped tables
@@ -59,7 +40,17 @@ DELIMITER ;
 -- Indexes for table `matches`
 --
 ALTER TABLE `matches`
-  ADD PRIMARY KEY (`matchId`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `matches`
+--
+ALTER TABLE `matches`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

773c690b09112e411d0ac4905760566166251724
sarunas <sarunas.palianskas@ktu.edu>
2022-09-26 20:18:26 +0300

    ui: add base player model

--- src/battleships-ui/src/models/Match.ts
+++ src/battleships-ui/src/models/Match.ts
@@ -1,4 +1,7 @@
+import { Player } from './Player';
+
 export class Match {
   id!: number;
   name!: string;
+  players!: Player[];
 }
---
+++ src/battleships-ui/src/models/Player.ts
@@ -0,0 +1,7 @@
+import { Match } from './Match';
+
+export class Player {
+  id!: number;
+  name!: string;
+  match!: Match;
+}

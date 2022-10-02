d3d5a4ed8b422afdbcc3050ee71b01e8c244664a
sarunas <sarunas.palianskas@ktu.edu>
2022-10-02 18:46:00 +0300

    api: fix event data getting corrupted during event propagation

--- src/battleships-api/Services/Hubs/MatchEventHub.cs
+++ src/battleships-api/Services/Hubs/MatchEventHub.cs
@@ -5,7 +5,7 @@ namespace Services.Hubs;
 public class MatchEventHub : Hub
 {
     // eventIds defined in FE
-    public void PropagateEvent(int eventId, string data)
+    public void PropagateEvent(int eventId, object data)
     {
         Clients.All.SendAsync("ReceiveEvent", eventId, data);
     }

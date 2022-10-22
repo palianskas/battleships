51e29d943bd4c38652b9d67a0c6634495f8ea1ca
sarunas <sarunas.palianskas@ktu.edu>
2022-10-22 10:01:22 +0300

    ui: remove deprecated MatchService parts

--- src/battleships-ui/src/services/MatchService/MatchService.ts
+++ src/battleships-ui/src/services/MatchService/MatchService.ts
@@ -16,35 +16,6 @@ const api = axios.create({
 });
 
 export class MatchService {
-  private static ENDPOINT_URL = 'matches/';
-  private static LOCAL_STORAGE_MATCH_KEY = 'MATCH';
-
-  static async get(): Promise<Match> {
-    const response = await api.get(this.ENDPOINT_URL);
-
-    return response.data;
-  }
-
-  static async createNew(name = 'New match'): Promise<Match> {
-    const response = await api.post(this.ENDPOINT_URL, { name: name });
-
-    return response.data;
-  }
-
-  static saveToLocalStorage(match: Match) {
-    localStorage.setItem(this.LOCAL_STORAGE_MATCH_KEY, JSON.stringify(match));
-  }
-
-  static getFromLocalStorage(): Match | null {
-    const value = localStorage.getItem(this.LOCAL_STORAGE_MATCH_KEY);
-
-    if (value == null) {
-      return null;
-    }
-
-    return JSON.parse(value);
-  }
-
   static initMatchPlayerVehicles(): void {
     const match = MatchProvider.Instance.match;
 

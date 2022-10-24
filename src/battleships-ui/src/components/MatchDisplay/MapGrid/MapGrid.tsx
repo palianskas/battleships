bf373ceaf7e83be0e3c26f5a25c1306d606a9ee8
sarunas <sarunas.palianskas@ktu.edu>
2022-10-24 17:52:22 +0300

    ui: add fog-of-war to enemy map display

--- src/battleships-ui/src/components/MatchDisplay/MapGrid/MapGrid.tsx
+++ src/battleships-ui/src/components/MatchDisplay/MapGrid/MapGrid.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import { MapTile } from '../../../models/MatchMap';
-import { Player } from '../../../models/Player';
+import { Player, PlayerTeam } from '../../../models/Player';
 import { ModularShipPart } from '../../../models/Ships/ShipPart';
 
 import './MapGrid.css';
@@ -13,9 +13,12 @@ interface MapGridProps {
 interface MapGridTileProps {
   tile: MapTile;
   onTileSelect: (tile: MapTile) => void;
+  isEnemyMap: boolean;
 }
 
 export default function MapGrid({ player, onTileSelect }: MapGridProps) {
+  const isEnemyMap = player.team == PlayerTeam.Red;
+
   return (
     <div className="w-100 d-flex justify-content-center">
       <div>
@@ -26,6 +29,7 @@ export default function MapGrid({ player, onTileSelect }: MapGridProps) {
                 <MapGridTile
                   tile={tile}
                   onTileSelect={onTileSelect}
+                  isEnemyMap={isEnemyMap}
                   key={idxY}
                 ></MapGridTile>
               );
@@ -37,22 +41,38 @@ export default function MapGrid({ player, onTileSelect }: MapGridProps) {
   );
 }
 
-function MapGridTile({ tile, onTileSelect }: MapGridTileProps) {
+function MapGridTile({ tile, onTileSelect, isEnemyMap }: MapGridTileProps) {
   let shipPartHpString =
-    tile.shipPart instanceof ModularShipPart
+    tile.shipPart instanceof ModularShipPart && !isEnemyMap
       ? (tile.shipPart as ModularShipPart).hp.toString()
       : '';
 
   return (
     <div
-      className={classNames('map-tile', {
-        blue: !!tile.shipPart,
-        grey: tile.isAttacked,
-        red: tile.isShipPartDestroyed,
-      })}
+      className={classNames('map-tile', resolveTileColorClass())}
       onClick={() => onTileSelect(tile)}
     >
       <span className="map-tile-hp-span">{shipPartHpString}</span>
     </div>
   );
+
+  function resolveTileColorClass(): string {
+    if (isEnemyMap && !tile.isAttacked) {
+      return '';
+    }
+
+    if (tile.isShipPartDestroyed) {
+      return 'red';
+    }
+
+    if (tile.shipPart) {
+      return 'blue';
+    }
+
+    if (tile.isAttacked) {
+      return 'grey';
+    }
+
+    return '';
+  }
 }

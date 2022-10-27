f489991e50f1df10862d6bd920cf77abe6dd8183
Sauvir <53517068+Sauvir@users.noreply.github.com>
2022-10-27 16:51:14 +0300

    added vehicle command pattern

---
+++ src/battleships-ui/src/services/Commands/VehicleCommand.ts
@@ -0,0 +1,50 @@
+﻿import Vehicle from '../../models/Vehicle';
+
+abstract class VehicleCommand {
+    protected vehicle: Vehicle;
+    protected lastPosition: { positionX: number, positionY: number }
+
+    protected constructor(vehicle: Vehicle) {
+        this.vehicle = vehicle;
+        this.lastPosition = { positionX: vehicle.positionX, positionY: vehicle.positionY }
+    }
+
+    saveLastPosition() {
+        this.lastPosition = { positionX: this.vehicle.positionX, positionY: this.vehicle.positionY }
+    }
+
+    undo() {
+        this.vehicle.positionX = this.lastPosition.positionX;
+        this.vehicle.positionY = this.lastPosition.positionY;
+    }
+
+    abstract execute(): Vehicle;
+}
+
+class MoveUp extends VehicleCommand {
+    execute() {
+        this.vehicle.positionY += 1
+        return this.vehicle
+    }
+}
+
+class MoveDown extends VehicleCommand {
+    execute() {
+        this.vehicle.positionY -= 1
+        return this.vehicle
+    }
+}
+
+class MoveRight extends VehicleCommand {
+    execute() {
+        this.vehicle.positionX += 1
+        return this.vehicle
+    }
+}
+
+class MoveLeft extends VehicleCommand {
+    execute() {
+        this.vehicle.positionX -= 1
+        return this.vehicle
+    }
+}

c73d996d7188a06259174859f1aef90626aa901f
Sauvir <53517068+Sauvir@users.noreply.github.com>
2022-11-03 22:24:52 +0200

    added adapter

---
+++ src/battleships-ui/src/services/Adapter/ShipToStringAdapter.ts
@@ -0,0 +1,16 @@
+﻿import Ship from "../../models/Ships/Ship";
+import LoggerService, { PatternTypes } from "../LoggerService/LoggerService";
+
+export class ShipToStringAdapter extends Ship {
+    public ship: Ship;
+    shipToString: string;
+    constructor(ship: Ship) {
+        super();
+        this.ship = ship;
+        this.shipToString = JSON.stringify(ship);
+    }
+    create() {
+        var logger = LoggerService.Instance.getLogger(PatternTypes.Adapter);
+        logger.log(`Ship to string: `, this.shipToString);
+    }
+}
